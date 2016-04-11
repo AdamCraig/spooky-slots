45861c73dc6494a39d6100ab860d4460690d2593
Adam Craig and Ashley Maceli <ashley@>
2016-04-11 15:17:10 -0700

    add rudimentary second reel functionality, cleaned up business logic by allowing spinReel to accept different reels as arguments

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
-function Machine (reel1, reel2, reel3) {
-  this.reel1 = ["skull", "candy", "ghost", "bat", "witch"];
+function Machine () {
+  this.reel1 = ["skull", "candy", "ghost", "bat", "witch", "pumpkins", "werewolf", "zombies", "vampires", "black cat", "grim reaper"];
+  this.reel2 = ["skull", "candy", "ghost", "bat", "witch", "pumpkins", "werewolf", "zombies", "vampires", "black cat", "grim reaper"];
 }
 
-Machine.prototype.spinReel = function() {
-  var reelStop = this.reel1[Math.floor(Math.random() * this.reel1.length)];
+Machine.prototype.spinReel = function(reel) {
+  var reelStop = reel[Math.floor(Math.random() * reel.length)];
   console.log(reelStop);
+
   return reelStop;
 }
 
+Machine.prototype.winningCombo = function() {
+
+}
 
 $(document).ready(function() {
 
@@ -16,7 +21,9 @@ $(document).ready(function() {
   $("#spin").click(function(event) {
     event.preventDefault();
 
-    $(".reel1output").text(newMachine.spinReel());
+    $(".reel1output").append(newMachine.spinReel(newMachine.reel1));
+    $(".reel2output").append(newMachine.spinReel(newMachine.reel2));
+    // $(".reel3output").append(newMachine.spinReels(reel3));
 
   });
 });
