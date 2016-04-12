cf49a765c61fa1d0f0aabf6b966c63e1bcb483d6
Adam Craig and Ashley Maceli <adam@>
2016-04-12 10:08:47 -0700

    add for loops and if statements to UI logic to append images for corresponding indexes in symbols array. Addsymbol images array. Add basic CSS to judge size of columns

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 // Business Logic
 
 function Machine () {
+  this.symbols = ["skull", "candy", "ghost", "bat", "witch", "pumpkins", "werewolf", "zombies", "vampires", "black cat", "grim reaper"];
+  this.symbolImages = ["skull.jpg", "candy.jpg", "ghost.jpg"];
   this.reel1 = ["skull", "candy", "ghost", "bat", "witch", "pumpkins", "werewolf", "zombies", "vampires", "black cat", "grim reaper"];
   this.reel2 = ["skull", "candy", "ghost", "bat", "witch", "pumpkins", "werewolf", "zombies", "vampires", "black cat", "grim reaper"];
   this.reel3 = ["skull", "candy", "ghost", "bat", "witch", "pumpkins", "werewolf", "zombies", "vampires", "black cat", "grim reaper"];
@@ -25,13 +27,27 @@ $(document).ready(function() {
   $("#spin").click(function(event) {
     event.preventDefault();
 
-    $(".reel1output").append(newMachine.spinReel(newMachine.reel1));
-    $(".reel2output").append(newMachine.spinReel(newMachine.reel2));
-    $(".reel3output").append(newMachine.spinReel(newMachine.reel3));
+    $(".reel1output").empty();
+    $(".reel2output").empty();
+    $(".reel3output").empty();
+
+    // $(".reel1output").text(newMachine.spinReel(newMachine.reel1));
+    // $(".reel2output").text(newMachine.spinReel(newMachine.reel2));
+    // $(".reel3output").text(newMachine.spinReel(newMachine.reel3));
 
     var reel1Result = newMachine.spinReel(newMachine.reel1);
     var reel2Result = newMachine.spinReel(newMachine.reel2);
     var reel3Result = newMachine.spinReel(newMachine.reel3);
+
+    for ( index = 0; index <= newMachine.symbols.length; index++ ) {
+      if (reel1Result === newMachine.symbols[index]) {
+        for ( i = 0; i < newMachine.symbolImages.length; i++) {
+          if ( index === i ) {
+            $(".reel1output").append("<img src='img/" + newMachine.symbolImages[index] + "'>");
+          }
+        }
+      }
+    }
 
     newMachine.winningCombo(reel1Result, reel2Result, reel3Result);
 
