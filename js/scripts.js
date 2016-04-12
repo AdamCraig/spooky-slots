83ffd1980116d25c1f8de34ae113b9b456f8e708
Nick Lyman and Tommy Jones <nick@>
2016-04-12 10:14:57 -0700

    Add winning combinations function prototype with values

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,52 @@ function Machine () {
   this.reel2 = ["skull", "candy", "ghost", "bat", "witch", "pumpkins", "werewolf", "zombies", "vampires", "black cat", "grim reaper"];
   this.reel3 = ["skull", "candy", "ghost", "bat", "witch", "pumpkins", "werewolf", "zombies", "vampires", "black cat", "grim reaper"];
 }
-
+ function Player () {
+   this.bankRoll = 20
+ }
 Machine.prototype.spinReel = function(reel) {
   var reelStop = reel[Math.floor(Math.random() * reel.length)];
   console.log(reelStop);
+
   return reelStop;
 }
-
+// Runs through winningReel arrary to check for winning combinations//
 Machine.prototype.winningCombo = function() {
       var winningReel = [];
-      for (i = 0)
+      winningReel[0] = reel1Result
+      for (i = 0; i <= winningReel.length; i++) {
+        if ( (winningReel[0] === "pumpkins") || (winningReel[1] === "pumpkins") || (winningReel[2] === "pumpkins") ) {
+          this.bankRoll += 2;
+      } if ( (winningReel[0] === "pumpkins" && winningReel[1] === "pumpkins") || (winningReel[0] === "pumpkins" && winningReel[2] === "pumpkins") || (winningReel[1] === "pumpkins" && winningReel[2] === "pumpkins") ) {
+          this.bankroll += 4;
+      } if ( (winningReel[0] === "pumpkins" && winningReel[1] === "pumpkins" && winningReel[2] === "pumpkins") ) {
+          this.bankroll += 12;
+      } if ( (winningReel[0] === "candy" && winningReel[1] === "candy") || (winningReel[0] === "candy" && winningReel[2] === "candy") || (winningReel[1] === "candy" && winningReel[2] === "candy") ) {
+          this.bankroll += 6;
+      } if ( (winningReel[0] === "candy" && winningReel[1] === "candy" && winningReel[2] === "candy") ) {
+          this.bankroll += 18;
+      } if ( (winningReel[0] === "black cat" && winningReel[1] === "black cat") || (winningReel[0] === "black cat" && winningReel[2] === "black cat") || (winningReel[1] === "black cat" && winningReel[2] === "black cat") ) {
+          this.bankroll += 12;
+      } if ( (winningReel[0] === "black cat" && winningReel[1] === "black cat" && winningReel[2] === "black cat") ) {
+          this.bankroll += 36;
+      } if ( (winningReel[0] === "witches" && winningReel[1] === "witches" && winningReel[2] === "witches") ) {
+          this.bankroll += 64;
+      } if ( (winningReel[0] === "ghosts" && winningReel[1] === "ghosts" && winningReel[2] === "ghosts") ) {
+          this.bankroll += 100;
+      } if ( (winningReel[0] === "bats" && winningReel[1] === "bats" && winningReel[2] === "bats") ) {
+          this.bankroll += 175;
+      } if ( (winningReel[0] === "werewolf" && winningReel[1] === "werewolf" && winningReel[2] === "werewolf") ) {
+          this.bankroll += 250;
+      } if ( (winningReel[0] === "vampires" && winningReel[1] === "vampires" && winningReel[2] === "vampires") ) {
+          this.bankroll += 500;
+      } if ( (winningReel[0] === "zombies" && winningReel[1] === "zombies" && winningReel[2] === "zombies") ) {
+          this.bankroll += 1000;
+      } if ( (winningReel[0] === "grim reaper" && winningReel[1] === "grim reaper" && winningReel[2] === "grim reaper") ) {
+          this.bankroll += 10000;
+      } else {
+        return (false);
+      }
+    }
 
 }
 
